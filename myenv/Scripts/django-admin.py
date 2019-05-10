dcce1713f1da2485095973757d9ff3253ef55099
HipolitoJr <hipolitocavalcantejr@gmail.com>
2019-05-09 22:52:05 -0300

    Initial commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Hipolito\Google Drive\ADS_IFPI\2019-1\ENGENHARIA DE SOFTWARE 2\classroom-ifpi\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
