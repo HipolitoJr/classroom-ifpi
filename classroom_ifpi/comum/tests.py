d887464ab711218fc262b9ffcad8703429586ed7
MayelleCarvalho <carvalhomayelle@gmail.com>
2019-05-17 08:31:21 -0300

    Implementação de teste unitario de criacao de curso

--- classroom_ifpi/comum/tests.py
+++ classroom_ifpi/comum/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from model_mommy import mommy
+from comum.models import Curso
 
-# Create your tests here.
+
+class TesteCurso(TestCase):
+
+   def setUp(self):
+       self.curso = mommy.make(Curso, descricao = 'CURSO DE TESTES EM PYTHON', tipo = 'TN')
+
+   def teste_curso_creation(self):
+       self.assertTrue(isinstance(self.curso, Curso))
