392b55b3d5a0e4a07340741b47c9fcec7b9fb6c1
paulorbsfilho <p4ulinho@outlook.com>
2019-06-26 18:37:36 -0300

    Inicio paginas 2

--- classroom_ifpi/comum/admin.py
+++ classroom_ifpi/comum/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Curso, Professor, Disciplina, Aluno, Turma, MatriculaDisciplinar, Horario  Aluno
+from .models import Curso, Professor, Disciplina, Aluno, Turma, MatriculaDisciplinar, Horario, Aluno
 
 
 @admin.register(Curso)
