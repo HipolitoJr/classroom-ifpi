bdb954957b83cf9f27ff3e53b67c6a46a4fbf263
Cicero Leonardo da Silva <clsinfsolution@gmail.com>
2019-07-12 11:49:14 -0300

    Update settings.py
    update settings

--- classroom_ifpi/classroom_ifpi/settings.py
+++ classroom_ifpi/classroom_ifpi/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'classroom_ifpi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
        'NAME': 'classroomdb',
@@ -101,13 +101,13 @@ WSGI_APPLICATION = 'classroom_ifpi.wsgi.application'
        'HOST': '127.0.0.1',
        'PORT': '5432',
   }
-}'''
-DATABASES = {
+}
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}'''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
