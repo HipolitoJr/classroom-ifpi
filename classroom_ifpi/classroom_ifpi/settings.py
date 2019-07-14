76cbd914d173ef3d2eeb5a95290b5683b5510735
infsolution <clsinfsolution@gmail.com>
2019-07-14 18:10:47 -0300

    update senha

--- classroom_ifpi/classroom_ifpi/settings.py
+++ classroom_ifpi/classroom_ifpi/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
        'ENGINE': 'django.db.backends.postgresql_psycopg2',
        'NAME': 'classroomdb',
        'USER': 'postgres',
-       'PASSWORD': 'marcelia',
+       'PASSWORD': 'postgres',
        'HOST': '127.0.0.1',
        'PORT': '5432',
   }
